3449cb8070bd07daa20b84c7f2d9d8c677c5ff60
Mogakamo <mogaka.amo254@gmail.com>
2021-07-13 17:37:00 +0300

    added the lighthouse_report(2021-07-13 1735hrs).json that has the light house report and also got to adopt some of the best practices

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "weather-icons/css/weather-icons.css"
 import "../styles/form.css"
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Head>
+          <title>Weather App</title>
+            <meta name="description" content="This is a weather app"/>
+        </Head>
+        <Component {...pageProps} />
+      </>
+
+  )
 }
 
 export default MyApp
