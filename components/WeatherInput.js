4e894d6aadbfdf2940f6099cbf24e5d1ed73b134
Mogakamo <mogaka.amo254@gmail.com>
2021-07-13 15:28:51 +0300

    added the css for the form

--- components/WeatherInput.js
+++ components/WeatherInput.js
@@ -4,13 +4,13 @@ const WeatherInput = () => {
     return (
         <div className="container">
             <div className="row">
-                <div className="col-md-3">
+                <div className="col-md-3 offset-md-2">
                     <input type="text" className="form-control" name="city" autoComplete="off" />
                 </div>
                 <div className="col-md-3">
                     <input type="text" className="form-control" name="country" autoComplete="off" />
                 </div>
-                <div className="col-md-3">
+                <div className="col-md-3 mt-md-0 text-md-left">
                     <button className="btn btn-warning">Get Weather</button>
                 </div>
             </div>
